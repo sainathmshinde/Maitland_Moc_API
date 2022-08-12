e91220213c989e4b29df94080865ac3a407cedbe
shindesainath692 <sainath.shinde@viselglobal.com>
2022-08-12 12:58:37 +0530

    api changes

--- data/getRoleWithUsers.js
+++ data/getRoleWithUsers.js
@@ -1,98 +1,103 @@
 module.exports = () => {
-  return [
-    {
-      role: { id: 1, name: "admin" },
-      users: [
-        {
-          id: "1",
-          userName: "John Doe",
-          firstName: "John",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "2",
-          userName: "Jane Do",
-          firstName: "Jane",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "3",
-          userName: "John Doe",
-          firstName: "John",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "4",
-          userName: "Jane Do",
-          firstName: "Jane",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "5",
-          userName: "John Doe",
-          firstName: "John",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "6",
-          userName: "Jane Do",
-          firstName: "Jane",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "7",
-          userName: "John Doe",
-          firstName: "John",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "8",
-          userName: "Jane Do",
-          firstName: "Jane",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "9",
-          userName: "John Doe",
-          firstName: "John",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "10",
-          userName: "Jane Do",
-          firstName: "Jane",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-      ],
-    },
-    {
-      role: { id: 2, name: "user" },
-      users: [
-        {
-          id: "1",
-          userName: "John Doe",
-          firstName: "John",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-        {
-          id: "2",
-          userName: "Jane Do",
-          firstName: "Jane",
-          lastName: "Doe",
-          dateOfBirth: "01/01/1990",
-        },
-      ],
-    },
-  ];
+  return {
+    items: [
+      {
+        role: { id: 1, name: "admin" },
+        users: [
+          {
+            id: "1",
+            userName: "John Doe",
+            firstName: "John",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "2",
+            userName: "Jane Do",
+            firstName: "Jane",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "3",
+            userName: "John Doe",
+            firstName: "John",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "4",
+            userName: "Jane Do",
+            firstName: "Jane",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "5",
+            userName: "John Doe",
+            firstName: "John",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "6",
+            userName: "Jane Do",
+            firstName: "Jane",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "7",
+            userName: "John Doe",
+            firstName: "John",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "8",
+            userName: "Jane Do",
+            firstName: "Jane",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "9",
+            userName: "John Doe",
+            firstName: "John",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "10",
+            userName: "Jane Do",
+            firstName: "Jane",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+        ],
+      },
+      {
+        role: { id: 2, name: "user" },
+        users: [
+          {
+            id: "1",
+            userName: "John Doe",
+            firstName: "John",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+          {
+            id: "2",
+            userName: "Jane Do",
+            firstName: "Jane",
+            lastName: "Doe",
+            dateOfBirth: "01/01/1990",
+          },
+        ],
+      },
+    ],
+    totalCount: 1000,
+    totalPages: 100,
+    currentPage: 1,
+  };
 };
--- data/getRoles.js
+++ data/getRoles.js
@@ -1,14 +1,19 @@
 module.exports = () => {
-  return [
-    { id: 1, name: "admin", description: "Description for the role" },
-    { id: 2, name: "user", description: "Description for the role" },
-    { id: 3, name: "guest", description: "Description for the role" },
-    { id: 4, name: "superuser", description: "Description for the role" },
-    { id: 5, name: "superadmin", description: "Description for the role" },
-    { id: 6, name: "Owner​​", description: "Description for the role" },
-    { id: 7, name: "contributor​", description: "Description for the role" },
-    { id: 8, name: "​custom Role​", description: "Description for the role" },
-    { id: 9, name: "​api User​", description: "Description for the role" },
-    { id: 10, name: "ui User​", description: "Description for the role" },
-  ];
+  return {
+    items: [
+      { id: 1, name: "admin", description: "Description for the role" },
+      { id: 2, name: "user", description: "Description for the role" },
+      { id: 3, name: "guest", description: "Description for the role" },
+      { id: 4, name: "superuser", description: "Description for the role" },
+      { id: 5, name: "superadmin", description: "Description for the role" },
+      { id: 6, name: "Owner​​", description: "Description for the role" },
+      { id: 7, name: "contributor​", description: "Description for the role" },
+      { id: 8, name: "​custom Role​", description: "Description for the role" },
+      { id: 9, name: "​api User​", description: "Description for the role" },
+      { id: 10, name: "ui User​", description: "Description for the role" },
+    ],
+    totalCoungt: 1000,
+    totalPages: 100,
+    currentPage: 1,
+  };
 };
