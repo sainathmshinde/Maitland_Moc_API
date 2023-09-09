96f6ef0f5b240ae073e5889cc0a66fd8fd5bfcba
Gangaprasad Mohite <gangaprasad.mohite@maitlandgroup.com>
2023-09-09 15:24:22 +0530

    api changes

---
+++ data/getPresetRateById.js
@@ -0,0 +1,13 @@
+module.export = () => {
+  return {
+    presetRateID: 93,
+    presetRateName: "Shangai Bank",
+    currentRate: 15.12,
+    userName: null,
+    checkerIds: "6494",
+    approverIds: "6497",
+    displayName: "Mayur Ambegaonkar",
+    presetRateApprovalStatus: "Awaiting Check",
+    maker: "myr",
+  };
+};
