b6d9d4182bf8450913f51da12f3525f1976cc95d
Gangaprasad Mohite <gangaprasad.mohite@maitlandgroup.com>
2024-01-17 15:32:15 +0530

    api changes

--- data/getValidationRules.js
+++ data/getValidationRules.js
@@ -2,19 +2,49 @@ module.exports = () => {
   return [
     {
       id: 1,
-      name: "Greater Than",
+      name: "ISO Currency Code",
       numberOfParameters: 1,
       parameterType: "number",
     },
     {
       id: 2,
-      name: "Less Than",
+      name: "Broker Exclusion",
       numberOfParameters: 1,
       parameterType: "number",
     },
     {
       id: 3,
-      name: "Between",
+      name: "Transaction Code Exclusion",
+      numberOfParameters: 2,
+      parameterType: "number",
+    },
+    {
+      id: 4,
+      name: "Not Null Or Blank",
+      numberOfParameters: 1,
+      parameterType: "number",
+    },
+    {
+      id: 5,
+      name: "Length",
+      numberOfParameters: 1,
+      parameterType: "number",
+    },
+    {
+      id: 6,
+      name: "Must Be Numeric",
+      numberOfParameters: 2,
+      parameterType: "number",
+    },
+    {
+      id: 7,
+      name: "Must Be DEMAT Or PAPER",
+      numberOfParameters: 1,
+      parameterType: "number",
+    },
+    {
+      id: 8,
+      name: "Apex Portfolio Static Data",
       numberOfParameters: 2,
       parameterType: "number",
     },
