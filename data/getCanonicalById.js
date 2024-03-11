8d6b7adfdb8009d1b7c7d5a339ea43bd77ebf591
Gangaprasad Mohite <gangaprasad.mohite@maitlandgroup.com>
2024-03-11 14:06:56 +0530

    api changes

--- data/getCanonicalById.js
+++ data/getCanonicalById.js
@@ -4,12 +4,12 @@ module.exports = () => {
       {
         fieldName: "2d", //name of field in canonical
         fieldValue1: "fdas", //value for the field
-        fieldValue2: "fsa", //extra data about the field
+        fieldValue2: "fsaf,f,a", //extra data about the field
       },
       {
         fieldName: "fdsa",
         fieldValue1: "uy",
-        fieldValue2: "kj",
+        fieldValue2: "kj,h,h",
       },
     ],
   };
